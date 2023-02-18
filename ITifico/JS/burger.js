44e58deedab37346a9b03f41dfa6f6f291df8d37
Алексей Картышев <aleksejkartyshev@gmail.com>
2023-02-18 17:40:39 +0300

    add burger

--- ITifico/JS/burger.js
+++ ITifico/JS/burger.js
@@ -7,12 +7,23 @@ function burger() {
     let dropdownMenu = document.querySelector(
       ".header-container__dropdown-menu"
     );
-    dropdownMenu.classList.toggle("open");
-    
-    console.log(burgerImg.src)
-    if (burgerImg.src !== "img/burger.svg") {
+    if (dropdownMenu.datasetOpen) {
+      dropdownMenu.datasetOpen = false
+      dropdownMenu.classList.remove("open");
+      dropdownMenu.classList.add("close");
+    } else {
+      dropdownMenu.datasetOpen = true
+      dropdownMenu.classList.add("open");
+      dropdownMenu.classList.remove("close");
+    }
+
+    console.log(burgerImg.src.split("/")[burgerImg.src.split("/").length - 1]);
+    if (
+      burgerImg.src.split("/")[burgerImg.src.split("/").length - 1] ===
+      "burger.svg"
+    ) {
       burgerImg.src = "img/burger-close.svg";
-    }else{
+    } else {
       burgerImg.src = "img/burger.svg";
     }
   });
